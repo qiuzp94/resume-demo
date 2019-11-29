bf0b86f5f10db4dc72a091580c74607866af5b7a
qiuzp <qiuzp94@foxmail.com>
2019-11-29 20:05:55 +0800

    完成下载简历按钮&技能条&作品集代码

---
+++ script.js
@@ -0,0 +1,10 @@
+//作品集bar状态控制器如下
+protfolio1.onclick=function(){
+    protfolioBar.className=("barInner state1")
+}
+protfolio2.onclick=function(){
+    protfolioBar.className=("barInner state2")
+}
+protfolio3.onclick=function(){
+    protfolioBar.className=("barInner state3")
+}
