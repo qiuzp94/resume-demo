7f0ce9957697b084cca9ab50de3fc49c0f900e79
qiuzp <qiuzp94@foxmail.com>
2019-12-17 18:18:56 +0800

    新增会动的二级菜单代码

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 //作品集bar状态控制器如下
-protfolio1.onclick=function(){
-    protfolioBar.className=("barInner state1")
+protfolio1.onclick = function () {
+    protfolioBar.className = ("barInner state1")
 }
-protfolio2.onclick=function(){
-    protfolioBar.className=("barInner state2")
+protfolio2.onclick = function () {
+    protfolioBar.className = ("barInner state2")
 }
-protfolio3.onclick=function(){
-    protfolioBar.className=("barInner state3")
+protfolio3.onclick = function () {
+    protfolioBar.className = ("barInner state3")
+}
+
+//会动的二级菜单
+let liTags = document.querySelectorAll('nav.menu > ul >li')
+console.log(liTags)
+for (let i = 0; i < liTags.length; i++) {
+    liTags[i].onmouseenter = function (x) {
+        x.currentTarget.classList.add('active')
+    }
+    liTags[i].onmouseleave = function (x) {
+        x.currentTarget.classList.remove('active')
+    }
 }
